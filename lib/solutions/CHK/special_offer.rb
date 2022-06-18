11030a89730c66d729b0629500bb08bb8abd3791
sbx_user1051 <sbx_user1051@169.254.170.37>
2022-06-18 21:02:37 +0000

    Sat Jun 18 21:02:37 UTC 2022

--- lib/solutions/CHK/special_offer.rb
+++ lib/solutions/CHK/special_offer.rb
@@ -1,10 +1,11 @@
 class SpecialOffer
-  attr_reader :qualifying_sku, :qualifying_quantity, :applied_total_price, :applied_sku
+  attr_reader :qualifying_sku, :qualifying_quantity, :applied_total_price, :applied_sku, :qualifying_quantity
 
-  def initialize(qualifying_sku, qualifying_quantity, applied_total_price, applied_sku = nil)
+  def initialize(qualifying_sku, qualifying_quantity, applied_total_price, applied_sku = nil, applied_quantity = nil)
     @qualifying_sku = qualifying_sku
     @qualifying_quantity = qualifying_quantity
     @applied_total_price = applied_total_price
     @applied_sku = applied_sku || @qualifying_sku
+    @applied_quantity = applied_quantity || @qualifying_quantity
   end
 end
