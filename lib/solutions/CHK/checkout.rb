c6876f94ddf56d7b9bad4a46e9c54cb9017773bc
sbx_user1051 <sbx_user1051@169.254.170.37>
2022-06-18 21:08:37 +0000

    Sat Jun 18 21:08:37 UTC 2022

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -6,19 +6,25 @@ require_relative 'special_offer'
 
 class Checkout
   def initialize()
-    @total = 0
-
     # Set up our store
-    @store = Store.new
-    @store.add_sku(SKU.new('A', 50, SpecialOffer.new(3, 130)))
-    @store.add_sku(SKU.new('B', 30, SpecialOffer.new(2, 45)))
-    @store.add_sku(SKU.new('C', 20))
-    @store.add_sku(SKU.new('D', 15))
+    a = SKU.new('A', 50)
+    b = SKU.new('B', 30)
+    c = SKU.new('C', 20)
+    d = SKU.new('D', 15)
+    e = SKU.new('E', 40)
+    s1 = SpecialOffer.new(a, 3, 130)
+    s2 = SpecialOffer.new(a, 5, 200)
+    s3 = SpecialOffer.new(b, 2, 45)
+    s4 = SpecialOffer.new(e, 2, 0, b, 1)
+
+    @store = Store.new([a, b, c, d, e], [s1, s2, s3, s4])
+
+    @basket = @store.skus.map{ |sku| [sku.name, 0] }.to_h
   end
 
   def checkout(skus)
     return 0 if skus.empty?
-    
+
     sku_char_array = skus.split('')
     sku_char_array.each do |sku_char|
       store_sku = @store.sku_in_store(sku_char)
@@ -29,3 +35,4 @@ class Checkout
     @store.skus.reject { |sku| sku.count.zero? }.map { |sku| sku.total_price }.reduce(:+)
   end
 end
+
