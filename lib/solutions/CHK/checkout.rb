a038dca9aa4e41e2743a0ec22e85c0154ad547b9
sbx_user1051 <sbx_user1051@169.254.137.93>
2022-06-18 19:18:10 +0000

    Sat Jun 18 19:18:10 UTC 2022

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,17 +1,29 @@
 # noinspection RubyUnusedLocalVariable
+class Store
+  def initialize(skus = [], special_offers = [])
+    @skus = skus
+    @special_offers = special_offers
+  end
+
+  def add_sku(sku)
+    @skus.append(sku)
+  end
+
+  def add_special_offer(special_offer)
+    @special_offers.append(special_offer)
+  end
+end
+
 class Checkout
-  def initialize(items = {A: 50, B: 30, C: 20, D: 15}, offers = {A: {quantity: 3, total_price: 130}, B: {quantity: 2, total_price: 45}})
+  def initialize(items = {A: 50, B: 30, C: 20, D: 15}, special_offers = {A: {quantity: 3, total_price: 130}, B: {quantity: 2, total_price: 45}})
     @items = items
     @offers = offers
   end
 
   def checkout(skus)
     total = 0
-    puts skus
     @items.each do |key, value|
       count = skus.scan(/(?=#{key})/).count || 0
-      puts 
-      puts count
       if @offers[key] && (count > @offers[key]['quantity'])
         total += offers[key][total_price]
       else
@@ -20,12 +32,24 @@ class Checkout
     end
     total
   end
-
 end
 
-# def SKU
-#   def initialize(name, price)
-#     @name = name
-#     @price = price
-#   end
-# end
+def SKU
+  def initialize(name, price)
+    @name = name
+    @price = price
+    @count = 0
+  end
+
+  def increment_count
+    @count += 1
+  end
+end
+
+def SpecialOffer
+  def initialize(sku, quantity, total_price)
+    @sku = sku
+    @quantity = quantity
+    @total_price = total_price
+  end
+end
