49520bdea67d09651ff0f449a7b0630e6bacec32
sbx_user1051 <sbx_user1051@169.254.170.37>
2022-06-18 22:17:37 +0000

    Sat Jun 18 22:17:37 UTC 2022

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -8,7 +8,7 @@ class Checkout
   def initialize()
     @store = Store.new
 
-    @basket = Hash.new(0)
+    @basket = Hash.new { |h, k| h[k] = { count: 0, discounts: 0 } }
   end
 
   def checkout(skus)
@@ -19,8 +19,34 @@ class Checkout
       store_sku = @store.sku_in_store(sku_char)
       return -1 unless store_sku
 
-      @basket[store_sku.name] += 1
+      @basket[store_sku][:count] += 1
     end
+    @basket.each { |sku, entry| calculate_discounts(sku, entry[:count]) }
+    process_offers
     @store.skus.reject { |sku| sku.count.zero? }.map { |sku| sku.total_price }.reduce(:+)
   end
+
+  def calculate_discounts(sku, count)
+    @store.special_offers.each do |offer|
+      if offer.applied_sku == sku && count >= offer.applied_quantity
+        if @basket[offer.qualifying_sku][:count] >= offer.qualifying_quantity
+          @basket[offer.qualifying_sku][:count] / offer.qualifying_quantity
+        end
+      end
+      
+
+      end
+        total += (count / offer.applied_quantity) * @special_offer.total_price
+        if  && count >= offer.qualifying_quantity && count >= offer.applied_quantity
+
+        else
+
+      end
+    end
+    total + (remaining_count * sku.price)
+  end
 end
+
+discounted_total = (@count / @special_offer.quantity) * @special_offer.total_price
+    regular_total = (@count % @special_offer.quantity) * @price
+    discounted_total + regular_total
