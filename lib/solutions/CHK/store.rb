d871fb856999b2c13402f2a878384613e175e0ea
sbx_user1051 <sbx_user1051@169.254.22.149>
2022-06-18 23:30:37 +0000

    Sat Jun 18 23:30:37 UTC 2022

--- lib/solutions/CHK/store.rb
+++ lib/solutions/CHK/store.rb
@@ -38,9 +38,19 @@ class Store
     s3 = SpecialOffer.new(b, 2, 45)
     s4 = SpecialOffer.new(e, 2, 0, b, 1)
     s5 = SpecialOffer.new(f, 2, 20, f, 3)
+    s6 = SpecialOffer.new(h, 5, 45)
+    s7 = SpecialOffer.new(h, 10, 80)
+    s8 = SpecialOffer.new(k, 2, 150)
+    s9 = SpecialOffer.new(n, 3, 0, m, 1)
+    s10 = SpecialOffer.new(p, 5, 200)
+    s11 = SpecialOffer.new(q, 5, 80)
+    s12 = SpecialOffer.new(r, 3, 0, q, 1)
+    s13 = SpecialOffer.new(u, 3, 120, u, 4)
+    s14 = SpecialOffer.new(v, 2, 90)
+    s15 = SpecialOffer.new(v, 3, 130)
 
     @skus = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
-    @special_offers = [s1, s2, s3, s4, s5]
+    @special_offers = [s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15]
   end
 
   def sku_in_store(sku_string)
