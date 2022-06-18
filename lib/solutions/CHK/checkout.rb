d4be351829501cd3080c12cf932420f1724e8a4f
sbx_user1051 <sbx_user1051@169.254.11.169>
2022-06-18 16:08:04 +0000

    Sat Jun 18 16:08:04 UTC 2022

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,10 +1,14 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
-  def initialise(items, offers)
+  def initialise(items = {A: 50, B: 30, C: 20, D: 15}, offers = {A: {quantity: 3, total_price: 130}, B: {quantity: 2, total_price: 45}})
+    @items = items
+    @offers = offers
   end
 
   def checkout(skus)
-    stripped_skus = skus.gsub /[^A-Z]/, ''
+    skus.gsub! /[^A-Z]/, ''
+
   end
 
 end
+
